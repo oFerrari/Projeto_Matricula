e4e7b97c1d157dda4ff9c220a0d861d07f1854d1
Andrei Ferrari Domingos <andrei.domingos@estudante.ifms.edu.br>
2023-05-25 21:25:38 -0400

    Subindo Projeto Base/

---
+++ src/main/java/br/edu/ifms/aula/curso/CursoForm.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifms.aula.curso;
+
+import br.edu.ifms.arch.dto.AdapterBaseObjectForm;
+import br.edu.ifms.aula.campus.CampusDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+/**
+ *
+ * @author nicho
+ */
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class CursoForm extends AdapterBaseObjectForm {
+    
+    public CampusDto campus;
+}
